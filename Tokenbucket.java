96b07df9c2c54c396c31754b5c91a22ac3db3110
zaid852 <122735357+zaid852@users.noreply.github.com>
2026-02-03 20:35:21 +0530

    Update and rename Token_bucket.java to Tokenbucket.java

--- Tokenbucket.java
+++ Tokenbucket.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.*;
 
-public class Token_bucket{
+public class Tokenbucket{
     public static void main(String args[]){
         Scanner in = new Scanner(System.in);
         int token_remaining=0;//total tokens in the bucket
