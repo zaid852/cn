b29b7f8d3ba385c5acc24751d17517cd535db32e
dum852 <122735357+zaid852@users.noreply.github.com>
2026-02-03 22:13:20 +0530

    Create UDPS.java

---
+++ UDPS.java
@@ -0,0 +1,24 @@
+import java.net.*;
+
+class UDPS {
+    public static void main(String args[]) throws Exception {
+        DatagramSocket serverSocket = new DatagramSocket(9876);
+        System.out.println("Server is Ready for the client");
+        byte[] receiveData = new byte[1024];
+        byte[] sendData;
+
+        while (true) {
+            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+            serverSocket.receive(receivePacket);
+            String sentence = new String(receivePacket.getData(), 0, receivePacket.getLength());
+            System.out.println("RECEIVED: " + sentence);
+
+            InetAddress IPAddress = receivePacket.getAddress();
+            int port = receivePacket.getPort();
+            String capitalizedSentence = sentence.toUpperCase();
+            sendData = capitalizedSentence.getBytes();
+            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+            serverSocket.send(sendPacket);
+        }
+    }
+}
