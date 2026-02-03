31b3fb8db2cd1a2ef01356d8eb22243be0620894
dum852 <122735357+zaid852@users.noreply.github.com>
2026-02-03 22:09:05 +0530

    Create UDPC.java

---
+++ UDPC.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.net.*;
+
+class UDPC {
+    public static void main(String[] args) throws Exception {
+        BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+        DatagramSocket clientSocket = new DatagramSocket();
+        InetAddress IPAddress = InetAddress.getByName("localhost");
+        byte[] sendData;
+        byte[] receiveData = new byte[1024];
+
+        System.out.println("Enter the string in lowercase so that you receive the message in Uppercase from the server");
+        String sentence = inFromUser.readLine();
+        sendData = sentence.getBytes();
+        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
+        clientSocket.send(sendPacket);
+
+        DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+        clientSocket.receive(receivePacket);
+        String modifiedSentence = new String(receivePacket.getData(), 0, receivePacket.getLength());
+        System.out.println("FROM SERVER: " + modifiedSentence);
+
+        clientSocket.close();
+    }
+}
